450d57bfa0db444eb30a9b13502844e7a15ceba9
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-15 01:42:25 +0500

    updated paths

--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -3,8 +3,8 @@ import {
     registerUser,
     loginUser,
     logoutUser,
-} from "../controllers/v1/user.controller";
-import { verifyJWT } from "../middlewares/v1/auth.middleware";
+} from "../../controllers/v1/user.controller";
+import { verifyJWT } from "../../middlewares/v1/auth.middleware";
 
 const router = Router();
 
