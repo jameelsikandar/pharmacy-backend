bcce8baf650afc76285c4159ced35c5b30b262e0
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 13:01:58 +0500

    user logics added

--- src/controllers/v1/user.controller.ts
+++ src/controllers/v1/user.controller.ts
@@ -3,8 +3,13 @@ import { User } from "../../models/user.models";
 import {
     registerUserSchema,
     loginSchema,
+    updateUserSchema,
 } from "../../validators/user.validator";
-import type { RegisterUser, LoginUser } from "../../validators/user.validator";
+import type {
+    RegisterUser,
+    LoginUser,
+    UpdateUser,
+} from "../../validators/user.validator";
 import { validateDto } from "../../utils/validateDto";
 import { generateToken } from "../../utils/jwt";
 import { asyncHandler } from "../../utils/asyncHandler";
@@ -12,37 +17,115 @@ import { ApiError } from "../../utils/ApiError";
 import { ApiResponse } from "../../utils/ApiResponse";
 import type { Types } from "mongoose";
 import { cookieOptions } from "../../constants/cookies.constants";
+import { uploadToCloudinary } from "../../utils/cloudinaryUpload";
+import fs from "fs";
+import type { AuthenticatedRequest } from "../../types/shared/IAuthenticatedRequest";
 
 // register user
 const registerUser = asyncHandler(async (req: Request, res: Response) => {
     const data = validateDto<RegisterUser>(registerUserSchema, req.body);
+
+    if (req.file) {
+        const response = await uploadToCloudinary(req.file.path);
+        if (!response || !response.secure_url) {
+            throw new ApiError(400, "Image uploading to Cloudinary failed!");
+        }
+        // console.log(response);
+        data.avatar = {
+            public_id: response.public_id,
+            secure_url: response.secure_url,
+        };
+
+        fs.unlinkSync(req.file.path); // delete file from server
+    }
 
     const existingUser = await User.findOne({ email: data.email });
     if (existingUser) throw new ApiError(400, "Email already exists!");
 
     const user = await User.create(data);
 
-    return new ApiResponse(201, user, "User Registered Successfully!").send(
-        res
-    );
+    return new ApiResponse(
+        201,
+        {
+            fullName: user.fullName,
+            avatar: user.avatar?.secure_url,
+            email: user.email,
+            contact: user.contact,
+        },
+        "User Registered Successfully!"
+    ).send(res);
 });
 
 //login user
 const loginUser = asyncHandler(async (req: Request, res: Response) => {
     const data = validateDto<LoginUser>(loginSchema, req.body);
 
-    const user = await User.findOne({ email: data.email }).select("+password");
-    if (!user) throw new ApiError(400, "User not found");
+    const user = await User.findOne({ email: data.email });
+    if (!user) throw new ApiError(400, "Invalid credentials");
 
     const passMatch = await user.comparePassword(data.password);
-    if (!passMatch) throw new ApiError(400, "Password is wrong");
+    if (!passMatch) throw new ApiError(400, "Invalid credentials");
 
     const token = generateToken((user._id as Types.ObjectId).toString());
-    const safeUser = await User.findById(user._id).select("-password");
 
     res.cookie("token", token, cookieOptions);
-    return new ApiResponse(200, { user: safeUser }, "Login success").send(res);
+    return new ApiResponse(
+        200,
+        {
+            Name: user.fullName,
+            Email: user.email,
+            Avatar: user.avatar?.secure_url,
+            Contact: user.contact,
+        },
+        "Login success"
+    ).send(res);
 });
+
+// update user
+
+const updateUser = asyncHandler(
+    async (req: AuthenticatedRequest, res: Response) => {
+        const data = validateDto<UpdateUser>(updateUserSchema, req.body);
+
+        const id = req.user?._id;
+
+        if (!id) {
+            throw new ApiError(401, "Unauthorized. Id not found!");
+        }
+
+        if (req.file) {
+            const response = await uploadToCloudinary(req.file.path);
+            if (!response || !response.secure_url) {
+                throw new ApiError(
+                    400,
+                    "Image uploading to Cloudinary failed!"
+                );
+            }
+            // console.log(response);
+            data.avatar = {
+                public_id: response.public_id,
+                secure_url: response.secure_url,
+            };
+
+            fs.unlinkSync(req.file.path); // delete file from server
+        }
+
+        const user = await User.findByIdAndUpdate(id, data, {
+            new: true,
+            runValidators: true,
+        }).select("-password");
+
+        if (!user) {
+            throw new ApiError(404, "User not found!");
+        }
+
+        return new ApiResponse(
+            200,
+            data,
+            "Credentials updated successfully!"
+        ).send(res);
+    }
+);
 
 // logout user
 const logoutUser = asyncHandler(async (req: Request, res: Response) => {
@@ -51,4 +134,4 @@ const logoutUser = asyncHandler(async (req: Request, res: Response) => {
     return new ApiResponse(200, {}, "Logout successfully!").send(res);
 });
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser, updateUser };
