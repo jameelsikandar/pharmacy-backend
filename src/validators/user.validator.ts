e23dc9f7919e95664e097c7217bb7bd06c72c3bd
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 12:10:46 +0500

    avatar schmea added

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,11 +1,26 @@
 import { z } from "zod";
 
+const avatarSchema = z.object({
+    public_id: z.string(),
+    secure_url: z.url(),
+});
+
 const registerUserSchema = z.object({
     fullName: z.string().min(2, "Name must be atleast 2 characters long"),
-    avatar: z.url("Must be valid avatar url").optional(),
     email: z.email("Invalid email address"),
     password: z.string().min(6, "Password must be atleast 6 characters long"),
     contact: z.string().optional(),
+    avatar: avatarSchema.optional(),
+});
+
+const updateUserSchema = z.object({
+    fullName: z
+        .string()
+        .min(2, "Name must be atleast 2 characters long")
+        .optional(),
+    email: z.email("Invalid email address").optional(),
+    contact: z.string().optional(),
+    avatar: avatarSchema.optional(),
 });
 
 const loginSchema = z.object({
@@ -13,7 +28,8 @@ const loginSchema = z.object({
     password: z.string(),
 });
 
-export { registerUserSchema, loginSchema };
+export { registerUserSchema, loginSchema, updateUserSchema };
 
 export type RegisterUser = z.infer<typeof registerUserSchema>;
 export type LoginUser = z.infer<typeof loginSchema>;
+export type UpdateUser = z.infer<typeof updateUserSchema>;
