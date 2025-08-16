8c07e236e93109bb3fad0a5684503994be0d16eb
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 23:02:48 +0500

    imports reanmed

--- src/middlewares/shared/multer.middleware.ts
+++ src/middlewares/shared/multer.middleware.ts
@@ -1,7 +1,7 @@
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
-import { ALLOWED_IMAGE_TYPES } from "../../constants/cookies.constants";
+import { ALLOWED_IMAGE_TYPES } from "../../constants/constants";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
