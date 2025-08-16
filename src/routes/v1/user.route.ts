085d4c4e01d34bb4ba5245f337c43d7cb18822c1
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 23:04:36 +0500

    add update, get route

--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -3,6 +3,8 @@ import {
     registerUser,
     loginUser,
     logoutUser,
+    updateUser,
+    getUserProfile,
 } from "../../controllers/v1/user.controller";
 import { authGuard } from "../../middlewares/v1/auth.guard";
 import { upload } from "../../middlewares/shared/multer.middleware";
@@ -10,11 +12,19 @@ import { upload } from "../../middlewares/shared/multer.middleware";
 const router = Router();
 
 // register user
-
 router.route("/register").post(upload.single("avatar"), registerUser);
 
+// login user
 router.route("/login").post(loginUser);
 
+// -------protected routes---------
+//update user
+router.route("/update").patch(authGuard, upload.single("avatar"), updateUser);
+
+// get user profile
+router.route("/profile").get(authGuard, getUserProfile);
+
+// logout user
 router.route("/logout").post(authGuard, logoutUser);
 
 export default router;
