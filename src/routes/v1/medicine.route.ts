068268be1fade09d86b5add8b4e9fa1c12eec5ae
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-17 00:59:20 +0500

    added list all medicines route

--- src/routes/v1/medicine.route.ts
+++ src/routes/v1/medicine.route.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authGuard } from '../../middlewares/v1/auth.guard';
 import {
+    listMedicines,
     addMedicine,
     updateMedicine,
     getMedicine,
@@ -11,15 +12,20 @@ import { upload } from '../../middlewares/shared/multer.middleware';
 const router = Router();
 
 // ------- protected troutes --------
+
+// get all medicines
+router.route('/').get(authGuard, listMedicines);
+
 //add medicine
 router.route('/add-medicine').post(authGuard, upload.single('image'), addMedicine);
 
 // update medicine
 router.route('/update/:medicineID').patch(authGuard, upload.single('image'), updateMedicine);
 
-// get medicine details
+// get medicine details by id
 router.route('/profile/:medicineID').get(authGuard, getMedicine);
 
 // delete medicine
 router.route('/delete/:medicineID').delete(authGuard, deleteMedicine);
+
 export default router;
