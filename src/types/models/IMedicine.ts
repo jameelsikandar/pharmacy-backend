5e1b470ceacf3db0998e32c941d1d52e91455e4d
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 23:05:03 +0500

    schema optimiazied

--- src/types/models/IMedicine.ts
+++ src/types/models/IMedicine.ts
@@ -1,16 +1,19 @@
 import { Types, Document } from "mongoose";
+import type { IImage } from "../shared/IImage";
+
+type MedicineCategory = "Tablet" | "Capsule" | "Syrup" | "Injection";
 
 interface IMedicineBase extends Document {
     name: string;
     price: number;
     stock: number;
-    dosage: number | string;
-    category: string;
-    image: string;
-    supplierID: Types.ObjectId;
+    dosage: string;
+    category: MedicineCategory;
+    image?: IImage;
+    supplierID?: Types.ObjectId;
 }
 
-export interface IMedicine extends IMedicineBase, Document {
+export interface IMedicine extends IMedicineBase {
     createdAt: Date;
     updatedAt: Date;
 }
