8c35ed00346985a6d76079b07db668b552d9e40e
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 23:05:37 +0500

    add update, add medicine

--- src/validators/medicine.validator.ts
+++ src/validators/medicine.validator.ts
@@ -1,16 +1,38 @@
 import { z } from "zod";
+import { imageSchema } from "./shared/imageSchema";
 
+// add medicine schema
 const addMedicineSchema = z.object({
     name: z.string().min(2, "Name must be at least 2 characters"),
-    price: z.number().min(0, "Price must be at least 0"),
-    stock: z.number().min(0, "Stock must be at least 0"),
+    price: z.coerce.number().min(0, "Price must be at least 0"),
+    stock: z.coerce.number().min(0, "Stock must be at least 0"),
     dosage: z.string().min(2, "Dosage must be at least 2 characters"),
-    category: z.string().min(1, "Category is required"),
-    image: z.string().url("Invalid image URL").optional(),
+    category: z.enum(["Tablet", "Capsule", "Syrup", "Injection"]),
+    image: imageSchema.optional(),
     supplierID: z
         .string()
         .regex(/^[0-9a-fA-F]{24}$/, "Invalid supplier ID")
         .optional(), // Mongo ObjectId
 });
 
-export { addMedicineSchema };
+// update medicine schema
+const updateMedicineSchema = z.object({
+    name: z.string().min(2, "Name must be at least 2 characters").optional(),
+    price: z.coerce.number().min(0, "Price must be at least 0").optional(),
+    stock: z.coerce.number().min(0, "Stock must be at least 0").optional(),
+    dosage: z
+        .string()
+        .min(2, "Dosage must be at least 2 characters")
+        .optional(),
+    category: z.enum(["Tablet", "Capsule", "Syrup", "Injection"]).optional(),
+    image: imageSchema.optional(),
+    supplierID: z
+        .string()
+        .regex(/^[0-9a-fA-F]{24}$/, "Invalid supplier ID")
+        .optional(), // Mongo ObjectId
+});
+
+export { addMedicineSchema, updateMedicineSchema };
+
+export type AddMedicine = z.infer<typeof addMedicineSchema>;
+export type UpdateMedicine = z.infer<typeof updateMedicineSchema>;
