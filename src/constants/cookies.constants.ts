6e8899cf02dad5c80d6efcd74c4e89d491d0280b
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-16 23:02:14 +0500

    renamed

--- src/constants/cookies.constants.ts
+++
@@ -1,9 +0,0 @@
-import { ENV } from "../config/env.config";
-
-// Cookie options for reusability
-export const cookieOptions = {
-    httpOnly: true,
-    secure: ENV.NODE_ENV === "production",
-    sameSite: "strict" as const,
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-};
