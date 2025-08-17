05b46d2a94ff8b4d5b5c9aba5536311d87e51735
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-17 15:57:31 +0500

    bug fixed

--- src/controllers/v1/medicine.controller.ts
+++ src/controllers/v1/medicine.controller.ts
@@ -1,14 +1,14 @@
-import type { Request, Response } from 'express';
-import { Medicine } from '../../models/medicine.model';
-import type { AddMedicine, UpdateMedicine } from '../../validators/medicine.validator';
-import { ApiResponse } from '../../utils/ApiResponse';
-import { addMedicineSchema, updateMedicineSchema } from '../../validators/medicine.validator';
-import { validateDto } from '../../utils/validateDto';
-import { uploadToCloudinary } from '../../utils/cloudinaryUpload';
-import { ApiError } from '../../utils/ApiError';
-import fs from 'fs';
-import { asyncHandler } from '../../utils/asyncHandler';
-import type { AuthenticatedRequest } from '../../types/shared/IAuthenticatedRequest';
+import type { Request, Response } from "express";
+import { Medicine } from "../../models/medicine.model";
+import type { AddMedicine, UpdateMedicine } from "../../validators/medicine.validator";
+import { ApiResponse } from "../../utils/ApiResponse";
+import { addMedicineSchema, updateMedicineSchema } from "../../validators/medicine.validator";
+import { validateDto } from "../../utils/validateDto";
+import { uploadToCloudinary } from "../../utils/cloudinaryUpload";
+import { ApiError } from "../../utils/ApiError";
+import fs from "fs/promises";
+import { asyncHandler } from "../../utils/asyncHandler";
+import type { AuthenticatedRequest } from "../../types/shared/IAuthenticatedRequest";
 
 // get all medicines
 const listMedicines = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
@@ -16,8 +16,8 @@ const listMedicines = asyncHandler(async (req: AuthenticatedRequest, res: Respon
 
     return new ApiResponse(
         200,
-        { 'Medicines: ': medicines },
-        'Medicines fetched successfully!',
+        { "Medicines: ": medicines },
+        "Medicines fetched successfully!",
     ).send(res);
 });
 
@@ -28,7 +28,7 @@ const addMedicine = asyncHandler(async (req: Request, res: Response) => {
     if (req.file) {
         const response = await uploadToCloudinary(req.file.path);
         if (!response || !response.secure_url) {
-            throw new ApiError(500, 'Image uploading to cloudinary failed!');
+            throw new ApiError(500, "Image uploading to cloudinary failed!");
         }
 
         data.image = {
@@ -36,8 +36,8 @@ const addMedicine = asyncHandler(async (req: Request, res: Response) => {
             secure_url: response.secure_url,
         };
 
-        await fs.unlink(req.file.path, (err) => {
-            console.log('Error while deleting image from server: ', err);
+        await fs.unlink(req.file.path).catch((err) => {
+            console.log("Error while deleting file from server", err);
         });
     }
     const uniqueQuery = {
@@ -50,12 +50,12 @@ const addMedicine = asyncHandler(async (req: Request, res: Response) => {
     const exists = await Medicine.exists(uniqueQuery);
 
     if (exists) {
-        throw new ApiError(409, 'Medicine already exists!');
+        throw new ApiError(409, "Medicine already exists!");
     }
 
     const medicine = await Medicine.create(data);
 
-    return new ApiResponse(201, medicine, 'Medicine added successfully!').send(res);
+    return new ApiResponse(201, medicine, "Medicine added successfully!").send(res);
 });
 
 // update medicine datails
@@ -64,13 +64,10 @@ const updateMedicine = asyncHandler(async (req: AuthenticatedRequest, res: Respo
 
     const data = validateDto<UpdateMedicine>(updateMedicineSchema, req.body);
 
-    console.log('MEDICINE ID: ', medicineID);
-    console.log('DATA: ', data);
-
     if (req.file) {
         const response = await uploadToCloudinary(req.file.path);
         if (!response || !response.secure_url) {
-            throw new ApiError(400, 'Image uploading to cloudinary failed!');
+            throw new ApiError(400, "Image uploading to cloudinary failed!");
         }
 
         data.image = {
@@ -78,8 +75,8 @@ const updateMedicine = asyncHandler(async (req: AuthenticatedRequest, res: Respo
             secure_url: response.secure_url,
         };
 
-        await fs.unlink(req.file.path, (err) => {
-            console.log('Error while deleting file from server ', err);
+        await fs.unlink(req.file.path).catch((err) => {
+            console.log("Error while deleting file from server", err);
         });
     }
 
@@ -88,18 +85,18 @@ const updateMedicine = asyncHandler(async (req: AuthenticatedRequest, res: Respo
             new: true,
             runValidators: true,
         });
-        console.log('MEDICINE: ', medicine);
 
         if (!medicine) {
-            throw new ApiError(404, 'Failed to update medicine');
+            throw new ApiError(404, "Failed to update medicine");
         }
 
-        return new ApiResponse(200, medicine, 'Medicine updated successfully!').send(res);
+        return new ApiResponse(200, data, "Medicine updated successfully!").send(res);
     } catch (error: any) {
         if (error.code === 11000) {
             throw new ApiError(
                 409,
-                'Medicine with same name, category, dosage and supplier already exists!',
+                "Medicine with same name, category, dosage and supplier already exists!",
+                error.keyValue,
             );
         }
         throw error;
@@ -111,16 +108,16 @@ const getMedicine = asyncHandler(async (req: AuthenticatedRequest, res: Response
     const { medicineID } = req.params;
 
     if (!medicineID) {
-        throw new ApiError(400, 'No medicine id.');
+        throw new ApiError(400, "No medicine id.");
     }
 
     const medicine = await Medicine.findById(medicineID);
 
     if (!medicine) {
-        throw new ApiError(404, 'Medicine not found');
+        throw new ApiError(404, "Medicine not found");
     }
 
-    return new ApiResponse(200, medicine, 'Medicine fetched successfully!').send(res);
+    return new ApiResponse(200, medicine, "Medicine fetched successfully!").send(res);
 });
 
 // delete medicine
@@ -130,17 +127,17 @@ const deleteMedicine = asyncHandler(async (req: AuthenticatedRequest, res: Respo
     const medicine = await Medicine.findByIdAndDelete(medicineID);
 
     if (!medicine) {
-        throw new ApiError(404, 'Medicine not found');
+        throw new ApiError(404, "Medicine not found");
     }
 
     return new ApiResponse(
         204,
         {
-            'Name: ': medicine.name,
-            'Category: ': medicine.category,
-            'Image: ': medicine.image?.secure_url,
+            "Name: ": medicine.name,
+            "Category: ": medicine.category,
+            "Image: ": medicine.image?.secure_url,
         },
-        'Medicine deleted successfully!',
+        "Medicine deleted successfully!",
     ).send(res);
 });
 
