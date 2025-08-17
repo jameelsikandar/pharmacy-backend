c8e1a304ba0a5c905ba5a18cfb6e71e05531654a
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-18 00:10:50 +0500

    image bug fixed

--- src/utils/cloudinaryUpload.ts
+++ src/utils/cloudinaryUpload.ts
@@ -1,11 +1,7 @@
 import { cloudinary } from "../config/cloudinary.config";
-import fs from "fs";
+import fs from "fs/promises";
 
-const uploadToCloudinary = async (
-    localFilePath: string,
-    maxRetries = 3,
-    delayMs = 1000
-) => {
+const uploadToCloudinary = async (localFilePath: string, maxRetries = 3, delayMs = 1000) => {
     if (!localFilePath) return null;
 
     let lastError: unknown;
@@ -15,32 +11,28 @@ const uploadToCloudinary = async (
             const response = await cloudinary.uploader.upload(localFilePath, {
                 resource_type: "auto",
             });
-            console.log(
-                `File uploaded to Cloudinary (attempt ${attempt}):`,
-                response.secure_url
-            );
+            console.log(`File uploaded to Cloudinary (attempt ${attempt}):`, response.secure_url);
+
+            await fs.unlink(localFilePath).catch((err) => {
+                console.log("Error while deleting image from server: ", err);
+            });
             return response;
         } catch (error) {
             lastError = error;
 
-            console.error(
-                `Cloudinary upload failed (attempt ${attempt}):`,
-                error
-            );
+            console.error(`Cloudinary upload failed (attempt ${attempt}):`, error);
 
             if (attempt < maxRetries) {
                 console.log(`Retrying in ${delayMs}ms...`);
                 await new Promise((res) => setTimeout(res, delayMs));
+            } else {
+                await fs.unlink(localFilePath).catch((err) => {
+                    console.log("Error while deleting image from server: ", err);
+                });
+
+                throw lastError;
             }
         }
-
-        try {
-            fs.unlinkSync(localFilePath);
-        } catch (unlinkError) {
-            console.error("Error deleting file from server:", unlinkError);
-        }
-
-        throw lastError;
     }
 };
 export { uploadToCloudinary };
