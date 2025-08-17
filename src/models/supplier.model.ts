5857011aae491d51b6025383e4a876be5b0d65c0
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-17 15:58:18 +0500

    made fields required

--- src/models/supplier.model.ts
+++ src/models/supplier.model.ts
@@ -4,13 +4,13 @@ import type { ISupplier } from "../types/models/ISupplier";
 const supplierSchema = new Schema<ISupplier>(
     {
         name: { type: String, required: true, trim: true },
-        email: { type: String, required: true, trim: true },
-        avatar: { type: String, required: false },
+        email: { type: String, required: true, trim: true, unique: true },
+        avatar: { public_id: { type: String }, secure_url: { type: String } },
         address: { type: String, required: true },
-        licenseNumber: { type: String, required: true },
+        licenseNumber: { type: String, required: true, unique: true },
         contact: { type: String, required: true },
     },
-    { timestamps: true }
+    { timestamps: true },
 );
 
 export const Supplier = mongoose.model<ISupplier>("Supplier", supplierSchema);
