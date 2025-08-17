c51f0984b5c7d73619aa009d2cc5f4003e541998
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-17 15:59:37 +0500

    added add, update, get supplier routes

---
+++ src/routes/v1/supplier.route.ts
@@ -0,0 +1,17 @@
+import express, { Router } from "express";
+import { addSupplier, updateSupplier, getSupplier } from "../../controllers/v1/supplier.controller";
+import { authGuard } from "../../middlewares/v1/auth.guard";
+import { upload } from "../../middlewares/shared/multer.middleware";
+
+const router = Router();
+
+// add supplier route
+router.route("/add").post(authGuard, upload.single("avatar"), addSupplier);
+
+// update supplier
+router.route("/update/:supplierID").patch(authGuard, upload.single("avatar"), updateSupplier);
+
+// get supplier details
+router.route("/:supplierID").get(authGuard, getSupplier);
+
+export default router;
