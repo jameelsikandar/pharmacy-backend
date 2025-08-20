00287df42f9cf9b231d0111f7c638a9747fdaf9d
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-21 00:56:37 +0500

    add Sale logic added

---
+++ src/controllers/v1/sale.controller.ts
@@ -0,0 +1,83 @@
+import type { AuthenticatedRequest } from "../../types/shared/IAuthenticatedRequest";
+import type { Response } from "express";
+import type { AddSale } from "../../validators/sale.validator";
+import { addSaleSchema } from "../../validators/sale.validator";
+import { Sale } from "../../models/sale.model";
+import { ApiError } from "../../utils/ApiError";
+import { ApiResponse } from "../../utils/ApiResponse";
+import { asyncHandler } from "../../utils/asyncHandler";
+import { Medicine } from "../../models/medicine.model";
+import { validateDto } from "../../utils/validateDto";
+import { Client } from "../../models/client.model";
+import type { Types } from "mongoose";
+import type { ISaleItem } from "../../types/models/ISale";
+
+// add sale
+const addSale = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+    const data = validateDto<AddSale>(addSaleSchema, req.body);
+
+    let clientId = data.clientId || null;
+
+    if (!clientId && data.fullName) {
+        // Check if client already exists by contact (to avoid duplicate)
+        let existingClient = null;
+        if (data.contact) {
+            existingClient = await Client.findOne({ contact: data.contact });
+        }
+
+        if (existingClient) {
+            clientId = (existingClient._id as Types.ObjectId).toString();
+        } else {
+            const newClient = await Client.create({
+                fullName: data.fullName,
+                contact: data.contact,
+                email: data.email, // optional
+            });
+            clientId = (newClient._id as Types.ObjectId).toString();
+        }
+    }
+
+    let total: number = 0;
+
+    const itemsToSave: ISaleItem[] = [];
+
+    for (const item of data.items) {
+        const medicine = await Medicine.findById(item.medicineId);
+        if (!medicine) {
+            throw new ApiError(400, "Medicine not found!");
+        }
+
+        if (medicine.stock < item.quantity) {
+            throw new ApiError(
+                400,
+                `Not enough stock for ${medicine.fullName}. Available ${medicine.stock}`,
+            );
+        }
+
+        const priceAtSale = medicine.price;
+        total += priceAtSale * item.quantity;
+
+        itemsToSave.push({
+            medicineId: medicine._id as Types.ObjectId,
+            quantity: item.quantity,
+            priceAtSale,
+        });
+
+        medicine.stock -= item.quantity;
+        await medicine.save();
+    }
+
+    const lastSale = await Sale.findOne().sort({ receiptNumber: -1 });
+    const receiptNumber = lastSale ? lastSale.receiptNumber + 1 : 1;
+
+    const sale = await Sale.create({
+        clientId,
+        items: itemsToSave,
+        total,
+        receiptNumber,
+    });
+
+    return new ApiResponse(201, sale, "Sale recorded successfully!").send(res);
+});
+
+export { addSale };
